7a32561d03b3f02ebb9fa88d57ef8506170b5f0c
Jonathan Kwak <jkwak09@gmail.com>
2019-10-20 19:48:08 -0700

    Complete indicator dot functionality

--- client/components/HomeCarousel.jsx
+++ client/components/HomeCarousel.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import '../styles/homeCarousel.css';
+import $ from 'jquery';
+
+//icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { faHeart as faHeartRegular } from '@fortawesome/free-regular-svg-icons';
+
 
 class HomeCarousel extends React.Component {
   constructor(props){
@@ -13,20 +20,18 @@ class HomeCarousel extends React.Component {
       id: this.props.id,
       slideOffset: 0,
       slidesTotal: this.props.photos.length,
-      slideWidth: 300, //I know, hardcoded, need to find a workaround
+      slideWidth: 300, //I know, hardcoded, need to find a fix
       slideMaxX: null,
+      controls: null,
       //dots
       indicatorDots: null,
       dotsOffset: 0,
       dotsindex: 0,
       dotsTotal: this.props.photos.length,
-      dotsWidth: 17, //I know, hardcoded, need to find a workaround
+      dotsWidth: 17, //I know, hardcoded, need to find a fix
       dotsMaxX: null,
       allDots: null,
       currDot: 0
-      // currentDot: null,
-      // endDotLeft: null,
-      // endDotRight: null,
 
     }
   // home
@@ -35,14 +40,17 @@ class HomeCarousel extends React.Component {
   this.moveDots = this.moveDots.bind(this);
   this.setupDots = this.setupDots.bind(this);
 
+  //click left and right
   this.nextClick = this.nextClick.bind(this);
   this.prevClick = this.prevClick.bind(this);
+
+
   }
 
   //for photos
   moveSlides(moveAmount) {
     this.state.homeCarousel.style.transform = 'translateX(-' + moveAmount + 'px)';
-    console.log(this.state.indicatorDots.children);
+    console.log(this.state.slidesWidth);
   };
 
   // for dots
@@ -61,12 +69,29 @@ class HomeCarousel extends React.Component {
       this.state.indicatorDots.children[this.state.currDot].classList.add('curr-dot');
     };
 
-    //secondary offset to keep track of index, if index is certain num, start real offset
+    //delay dots movement to keep in center
     if (this.state.dotsOffset !== this.state.dotsMaxX) {
       if (this.state.currDot >= 4) {
         if (this.state.currDot <=  (this.state.dotsTotal - 4)) {
           this.state.dotsOffset += this.state.dotsWidth;
           this.moveDots(this.state.dotsOffset);
+
+          if(this.state.indicatorDots.children[(this.state.currDot-4)].classList.contains('small-dots')) {
+            this.state.indicatorDots.children[(this.state.currDot-4)].classList.remove('small-dots')
+          };
+
+          if(this.state.indicatorDots.children[(this.state.currDot+2)].classList.contains('small-dots')) {
+            this.state.indicatorDots.children[(this.state.currDot+2)].classList.remove('small-dots')
+          };
+
+          this.state.indicatorDots.children[(this.state.currDot-3)].classList.add('small-dots');
+          this.state.indicatorDots.children[(this.state.currDot+3)].classList.add('small-dots');
+
+          if(this.state.currDot === (this.state.dotsTotal - 4)) {
+            if(this.state.indicatorDots.children[this.state.dotsTotal-1].classList.contains('small-dots')) {
+              this.state.indicatorDots.children[this.state.dotsTotal-1].classList.remove('small-dots')
+            };
+          };
         };
       };
     };
@@ -82,11 +107,30 @@ class HomeCarousel extends React.Component {
       this.state.currDot -= 1;
       this.state.indicatorDots.children[this.state.currDot].classList.add('curr-dot');
     };
+
+    //delay dots movement to keep in center
     if (this.state.dotsOffset !== 0) {
       if (this.state.currDot >= 3) {
         if (this.state.currDot <=  (this.state.dotsTotal - 5)) {
           this.state.dotsOffset -= this.state.dotsWidth;
           this.moveDots(this.state.dotsOffset);
+
+          if(this.state.indicatorDots.children[(this.state.currDot-2)].classList.contains('small-dots')) {
+            this.state.indicatorDots.children[(this.state.currDot-2)].classList.remove('small-dots')
+          };
+
+          if(this.state.indicatorDots.children[(this.state.currDot+4)].classList.contains('small-dots')) {
+            this.state.indicatorDots.children[(this.state.currDot+4)].classList.remove('small-dots')
+          };
+
+          this.state.indicatorDots.children[(this.state.currDot-3)].classList.add('small-dots');
+          this.state.indicatorDots.children[(this.state.currDot+3)].classList.add('small-dots');
+
+          if(this.state.currDot === 3) {
+            if(this.state.indicatorDots.children[0].classList.contains('small-dots')) {
+              this.state.indicatorDots.children[0].classList.remove('small-dots')
+            };
+          }
         };
       };
     };
@@ -94,14 +138,17 @@ class HomeCarousel extends React.Component {
 
   setupDots() {
     this.state.indicatorDots.children[0].classList.add('curr-dot');
+    this.state.indicatorDots.children[6].classList.add('small-dots');
   }
+
+
 
   componentDidMount() {
     this.setState({
       homeCarousel: document.querySelector('.home-carousel'+this.props.id),
       prevButton: document.querySelector('.home-carousel-button-prev'),
       nextButton: document.querySelector('.home-carousel-button-next'),
-      // slideWidth: document.querySelector('.slide').style.width,
+      // slideWidth: setTimeout(() => {document.querySelector('.slide').style.width}, 0),
       slideMaxX: this.state.slideWidth * (this.state.slidesTotal - 1),
 
       indicatorDots: document.querySelector('.indicator-dots'+this.props.id),
@@ -109,9 +156,13 @@ class HomeCarousel extends React.Component {
 
     });
 
-    setTimeout(()=>{this.setupDots()}, 0);
+    setTimeout(() => {this.setupDots()}, 0);
 
-
+    $(".home-button-wrapper").hover(function(){
+      $(this).css("opacity", "1");
+      }, function(){
+      $(this).css("opacity", "0");
+    });
   }
 
 
@@ -131,9 +182,13 @@ class HomeCarousel extends React.Component {
           }
 
         </ul>
-        <div className="button-wrapper">
-          <div className="home-carousel-button-prev" onClick={this.prevClick}>L</div>
-          <div className="home-carousel-button-next" onClick={this.nextClick}>R</div>
+        <div className="home-button-wrapper" >
+          <div className="home-carousel-button-prev" onClick={this.prevClick}>
+            <FontAwesomeIcon icon={faChevronLeft} />
+          </div>
+          <div className="home-carousel-button-next" onClick={this.nextClick}>
+            <FontAwesomeIcon icon={faChevronRight} />
+          </div>
         </div>
         <div className="indicator-wrapper">
           <ul className={'indicator-dots'+this.props.id+' indicator-dots'}>
@@ -145,6 +200,11 @@ class HomeCarousel extends React.Component {
             )}
           </ul>
           </div>
+          <div className="favorite">
+            <FontAwesomeIcon icon={faHeartRegular} />
+          </div>
+
+          {/* <div className="login-modal modal-shadow"></div> */}
       </div>
     )
   }
