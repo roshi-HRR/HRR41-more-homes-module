498918bd6816fcb879ad475e5e66757c237654e8
Jonathan Kwak <jkwak09@gmail.com>
2019-10-20 19:49:40 -0700

    Add css loader

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,13 @@ module.exports = {
             presets: ['@babel/preset-react', '@babel/preset-env']
           }
         }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
       }
     ]
   },
